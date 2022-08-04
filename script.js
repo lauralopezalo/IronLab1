75fccb63bae5421e57e12f93c8d4978f8e416310
Laura López Alonso <laura.lopez.alo@gmail.com>
2022-08-04 18:31:19 +0200

    cumplimentar las tarjetas de Recent Proyects

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Hmaburger menu---------------------------------------------------------------
+
 const menu = document.querySelector(".smartphone-menu");
 const menuItems = document.querySelectorAll(".menu-item");
 const hamburger = document.querySelector(".hamburger");
@@ -27,3 +29,45 @@ menuItems.forEach(
       menuItem.addEventListener("click", toggleMenu);
     }
   )
+
+  //---------------------------------------------------------------------------
+
+// Rellenar las fichas de Recent Projects --------------------------
+
+
+   async function getData() {
+    let url = [`http://jsonplaceholder.typicode.com/posts/?_limit=3`]
+     try {
+         let res = await fetch(url);
+         return await res.json();
+     } catch (error) {
+         console.log(error);
+     }
+ }
+ async function fillCards() {
+     let info = await getData();
+     let dataCard = "";
+     info.forEach((item) => {
+         let cardInfo = `<div class="project-target-container">
+                          <div class="project-target">
+                                <img
+                                    src="https://res.cloudinary.com/dflnp8bry/image/upload/v1659600360/IronLab1/Fondos/Colorful%20shape%20${item.id}.jpg"
+                                    alt="imagen"
+                                    class="project-img"
+                                />
+                              <div class="single-target">
+                                  <h5  class="title-target">${item.title} ${item.title}</h5>
+                                  <p class="body-target">${item.body}</p>
+                                  <a href="projects.html" class="link">Learn more</a>
+                              </div>
+                              </div>
+                         </div>`;
+         return dataCard += cardInfo;
+     })
+     let container = document.querySelector(".project-target-container");
+     container.innerHTML = dataCard;
+ }
+
+ fillCards()
+
+
